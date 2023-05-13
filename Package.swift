1ace7bc35428ea99501016b45377f19c31d25467
tiennv166 <tiennv166@gmail.com>
2023-05-13 10:49:55 +0700

    add package

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "CompositionalGridView",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "CompositionalGridView", targets: ["CompositionalGridView"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "6.0.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "12.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "CompositionalGridView",
+            path: "CompositionalGridView/Classes"
+        ),
+        .testTarget(
+            name: "CompositionalGridViewTests",
+            dependencies: [
+                "CompositionalGridView",
+                .product(name: "Quick", package: "Quick"),
+                .product(name: "Nimble", package: "Nimble")
+            ],
+            path: "Example/Tests"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
